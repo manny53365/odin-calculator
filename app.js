8bebe6a9c401d5abc0c4e9cda551908edb3dee6f
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-10-25 00:47:19 +0530

    added remaining buttons and event handlers to make the calculator work

--- app.js
+++ app.js
@@ -1,77 +1,98 @@
 //global vars section
-let firstNum, secondNum, operator = '';
+let firstNum, secondNum, operator = '', shouldResetScreen = false;
 
 // mapping DOM buttons to vars
 const numberButtons = document.getElementsByClassName('btn-secondary');
 const operatorButtons = document.getElementsByClassName('btn-warning');
+const equalButton = document.getElementById('eqlBtn');
 const clearButton = document.getElementById('clearBtn');
-const lastOperationScreen = document.getElementById('lastOperationScreen')
-const currentOperationScreen = document.getElementById('currentOperationScreen')
+const deleteButton = document.getElementById('deleteBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen');
+const currentOperationScreen = document.getElementById('currentOperationScreen');
 
-function updateDisplay(event) {
-    const buttonValue = event.target.textContent;
-
-    const display = document.getElementById('currentOperationScreen');
-
-    display.textContent === '0' ? display.textContent = buttonValue : display.textContent += buttonValue;
-};
-
-const convertOperator = (selectedOperator) => {
-    if (selectedOperator !== null && selectedOperator !== "") calculate();
-    firstNum = currentOperationScreen.textContent;
-    operator = selectedOperator;
-    lastOperationScreen.textContent = `${firstNum} ${operator}`;
-    currentOperationScreen.textContent = 0;
+const appendNumber = (number) => {
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen) resetScreen();
+    currentOperationScreen.textContent += number;
 }
 
-const calculate = () => {
-    if (operator === null) return
-    if (operator === '÷' && currentOperationScreen.textContent === '0') alert("You can't divide by 0!");
-    secondNum = currentOperationScreen.textContent
-    currentOperationScreen.textContent = round(
-        operate(operator, firstNum, secondNum)
-    )
-    lastOperationScreen.textContent = `${firstNum} ${operator} ${secondNum} =`
+const resetScreen = () => {
+    currentOperationScreen.textContent = '';
+    shouldResetScreen = false;
+}
+
+const clear = () => {
+    currentOperationScreen.textContent = '0'
+    lastOperationScreen.textContent = ''
+    firstNum = ''
+    secondNum = ''
     operator = null
 }
 
-const round = (total) => Math.round(total * 1000) / 1000;
+const appendPoint = () => {
+    if (shouldResetScreen) resetScreen();
+    if (currentOperationScreen.textContent === '');
+        currentOperationScreen.textContent = '0';
+    if (currentOperationScreen.textContent.includes('.')) return;
+    currentOperationScreen.textContent += '.';
+}
 
-function clear() {
-    currentOperationScreen.textContent = '0';
-    lastOperationScreen.textContent = '';
-    firstOperand = '';
-    secondOperand = '';
-    currentOperation = null;
+const deleteNumber = () => currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
+
+const setOperation = (selectedOperator) => {
+    if (selectedOperator !== null) calculate();
+    firstNum = currentOperationScreen.textContent;
+    operator = selectedOperator;
+    lastOperationScreen.textContent = `${firstNum} ${operator}`;
+    shouldResetScreen = true;
+}
+
+const calculate = () =>  {
+    if (operator === null || shouldResetScreen) return;
+    if (operator === '÷' && currentOperationScreen.textContent === '0') alert("You can't divide by 0!");
+    secondNum = currentOperationScreen.textContent
+    currentOperationScreen.textContent = round(
+        operate(firstNum, operator, secondNum)
+    );
+    lastOperationScreen.textContent = `${firstNum} ${operator} ${secondNum} =`;
+    operator = null;
+}
+
+const round = (number) => Math.round(number * 1000) / 1000;
+
+const handleKeyboardInput = (event) => {
+    if (event.key >= 0 && event.key <= 9) appendNumber(event.key);
+    if (event.key === '.') appendPoint();
+    if (event.key === '=' || event.key === 'Enter') evaluate();
+    if (event.key === 'Backspace') deleteNumber();
+    if (event.key === 'Escape') clear();
+    if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/');
+        setOperation(convertOperator(event.key));
+}
+
+const convertOperator = (keyboardOperator) => {
+    if (keyboardOperator === '/') return '÷';
+    if (keyboardOperator === '*') return '×';
+    if (keyboardOperator === '-') return '−';
+    if (keyboardOperator === '+') return '+';
 }
 
 for (const button of numberButtons) {
-    button.addEventListener('click', updateDisplay);
+    button.addEventListener('click', () => appendNumber(button.textContent));
 }
 
 for (const operatorButton of operatorButtons) {
-    operatorButton.addEventListener('click', () => convertOperator(operatorButton.textContent));
+    operatorButton.addEventListener('click', () => setOperation(operatorButton.textContent));
 };
 
-clearButton.addEventListener('click', clear)
+clearButton.addEventListener('click', clear);
+equalButton.addEventListener('click', calculate);
 
 // math operations
 
-const add = (a, b) => {
-    return a + b;
-}
-
-const subtract = (a, b) => {
-    return a - b;
-}
-
-const multiply = (a, b) => {
-    return a * b;
-}
-
-const divide = (a, b) => {
-    return a / b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 // This function will parse the parameters passed in and call the add, subtract, multiply or divide function
 const operate = (firstNum, operator, secondNum) => {
@@ -92,3 +113,4 @@ const operate = (firstNum, operator, secondNum) => {
             return null;
     }
 }
+
