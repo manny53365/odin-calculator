6e7c11499857095fb0bdc1c90011f11c39e46441
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-10-24 14:21:07 +0530

    added add, subtract, multiply and divide functions

---
+++ app.js
@@ -0,0 +1,21 @@
+//global vars section
+
+
+// math operations
+
+const add = (a,b) => {
+    return a + b;
+}
+
+const subtract = (a,b) => {
+    return a - b; 
+}
+
+const multiply = (a,b) => {
+    return a * b;
+}
+
+const divide = (a,b) => {
+    return a / b;
+}
+
